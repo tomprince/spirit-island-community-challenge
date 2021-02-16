f4eadba97dcaac898d06b12f0f823e85b8ab2d3f
Tom Prince <tom.prince@hocat.ca>
2021-02-16 11:14:57 -0700

    Initial.

---
+++ generate.py
@@ -0,0 +1,18 @@
+import json
+from pathlib import Path
+
+INDEX = Path("index.json")
+GENERATED = Path("generated.json")
+
+index = json.loads(INDEX.read_text(encoding="utf-8"))
+generated = {}
+for week in index:
+    variants = {}
+    path = Path(str(week['number']))
+    for name in week['variants']:
+        variant_path = path.joinpath(name + ".json")
+        variants[name] = json.loads(variant_path.read_text(encoding="utf-8"))
+    week['variants'] = variants
+    week['variantNames'] = list(variants.keys())
+
+GENERATED.write_text(json.dumps(index, ensure_ascii=False))
